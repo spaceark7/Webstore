0cefee6035af87257b02531a414a32dfcab76217
SpaceArk7 <spacearkchief@gmail.com>
2021-02-02 22:47:10 +0700

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Nav } from 'react-bootstrap'
+import Navigation from './Components/Navigation.js'
+
+function App() {
+  return (
+    <div className='App'>
+      <Navigation />
+      <main>
+        <Container></Container>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navbar, Nav, Button, Container } from 'react-bootstrap'
+
+const Navigation = () => {
+  return (
+    <div className='header w-100 h-100'>
+      <Navbar variant='dark' bg='primary' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Web Store</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>Cart</Nav.Link>
+              <Nav.Link href='/signin'>Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Navigation
